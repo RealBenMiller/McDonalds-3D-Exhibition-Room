3015ead5ee0ed31fce71e3b3984b8d68d7bcebf2
RealBenMiller <b.r.miller@hotmail.co.uk>
2021-01-31 17:21:00 +0000

    1

--- index.js
+++ index.js
@@ -31,30 +31,23 @@ var levels = [
   { size: 2048, tileSize: 512 },
 ];
 
+vvar levels = [
+  { tileSize: 512, size: 512 },
+  { tileSize: 512, size: 1024 }
+];
+
 var geometry = new Marzipano.CubeGeometry(levels);
-var source = Marzipano.ImageUrlSource.fromString(
-  "tiles/0-mcdonalds-exhibition-room/{z}/{f}/{y}/{x}.jpg",
-  { cubeMapPreviewUrl: "tiles/0-mcdonalds-exhibition-room/preview.jpg" }
-);
-
-var initialView = {
-  yaw: (90 * Math.PI) / 180,
-  pitch: (-30 * Math.PI) / 180,
-  fov: (90 * Math.PI) / 180,
-};
-
-var limiter = Marzipano.RectilinearView.limit.traditional(
-  1024,
-  (120 * Math.PI) / 180
-);
-
-var view = new Marzipano.RectilinearView(initialView, limiter);
+var source = Marzipano.ImageUrlSource.fromString("tiles/{z}/{f}/{y}/{x}.jpg");
+var view = new Marzipano.RectilinearView();
 
 var scene = viewer.createScene({
   source: source,
   geometry: geometry,
-  view: view,
-  pinFirstLevel: true,
+  view: view
+});
+
+scene.switchTo({
+  transitionDuration: 1000
 });
 
 // (function() {
