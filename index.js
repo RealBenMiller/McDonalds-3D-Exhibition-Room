898b176c69878a70443db65647a1ee1351846c61
RealBenMiller <b.r.miller@hotmail.co.uk>
2021-01-31 17:17:10 +0000

    1

--- index.js
+++ index.js
@@ -18,29 +18,44 @@
 var panoElement = document.getElementById("pano");
 var viewerOpts = {
   controls: {
-    mouseViewMode: "drag", // drag|qtvr
+    mouseViewMode: "drag",
   },
 };
 
 var viewer = new Marzipano.Viewer(panoElement, viewerOpts);
 
 var levels = [
-  { tileSize: 512, size: 512 },
-  { tileSize: 512, size: 1024 },
+  { tileSize: 256, size: 256, fallbackOnly: true },
+  { size: 512, tileSize: 512 },
+  { size: 1024, tileSize: 512 },
+  { size: 2048, tileSize: 512 },
 ];
 
-var geometry = new Marzipano.CubeGeometry(levels);
-var source = Marzipano.ImageUrlSource.fromString("tiles/{z}/{f}/{y}/{x}.jpg");
-var view = new Marzipano.RectilinearView();
+var geometry = new Marzipano.CubeGeometry(levles);
+var source = Marzipano.ImageUrlSource.fromString(
+  "tiles/0-mcdonalds-exhibition-room/{z}/{f}/{y}/{x}.jpg",
+  { cubeMapPreviewUrl: "tiles/0-mcdonalds-exhibition-room/preview.jpg" }
+);
+
+var initialView = {
+  yaw: (90 * Math.PI) / 180,
+  pitch: (-30 * Math.PI) / 180,
+  fov: (90 * Math.PI) / 180,
+};
+
+var limiter = Marzipano.RectilinearView.limit.traditional(
+  1024,
+  (120 * Math.PI) / 180
+);
+
+var view = new Marzipano.RectilinearView(initialView, limiter);
 
 var scene = viewer.createScene({
   source: source,
   geometry: geometry,
   view: view,
+  pinFirstLevel: true,
 });
-
-// Display scene.
-scene.switchTo({ transitionDuration: 1000 });
 
 // (function() {
 //   var Marzipano = window.Marzipano;
