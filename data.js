2cf769e7fcfeb12fde08a04b9f885dc5515e9c6b
RealBenMiller <b.r.miller@hotmail.co.uk>
2021-01-31 16:29:43 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-mcdonalds-exhibition-room",
+      "name": "MCDONALDS EXHIBITION ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.02655209632063915,
+        "pitch": 0,
+        "fov": 1.5490757487700753
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.00752894003238147,
+          "pitch": 0.026299914993902718,
+          "title": "TV",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7227102907804497,
+          "pitch": -0.06692682398677086,
+          "title": "Board One",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.3274772615518735,
+          "pitch": -0.09375995713757135,
+          "title": "Board Two",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.148535838050851,
+          "pitch": -0.0883654887078631,
+          "title": "Board Three",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.7974038372694032,
+          "pitch": 0.20617291567136675,
+          "title": "Leaflet",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.1409517927426105,
+          "pitch": -0.11335045794850629,
+          "title": "Board Four",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.3806520833866394,
+          "pitch": -0.1250543884233064,
+          "title": "Board Five",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7455772111502839,
+          "pitch": -0.09910901785368864,
+          "title": "Board Six",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
